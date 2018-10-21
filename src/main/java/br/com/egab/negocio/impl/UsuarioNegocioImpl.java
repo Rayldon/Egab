ee3456e19bf3395ecc85da80cd0df45441c9656e
Rayldon <r4yldon@gmail.com>
2018-10-21 12:16:46 -0200

    teste

--- src/main/java/br/com/egab/negocio/impl/UsuarioNegocioImpl.java
+++ src/main/java/br/com/egab/negocio/impl/UsuarioNegocioImpl.java
@@ -27,7 +27,7 @@ public class UsuarioNegocioImpl implements UsuarioNegocio{
 	
 	@Override
 	public void excluir(Long id){
-		System.out.println("master");
+		System.out.println("master2");
 		usuarioPersistencia.getEntityManager().remove(buscarPorId(id));
 	}
 	
