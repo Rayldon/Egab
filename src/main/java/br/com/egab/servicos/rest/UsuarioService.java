be166259f1a7d5a912d0dff60e6108fc3831cd2e
Rayldon <r4yldon@gmail.com>
2018-11-04 18:03:15 -0200

    teste2

--- src/main/java/br/com/egab/servicos/rest/UsuarioService.java
+++ src/main/java/br/com/egab/servicos/rest/UsuarioService.java
@@ -44,7 +44,7 @@ public class UsuarioService {
 	
 	@POST
 	@Path("/salvar2")
-	public Usuario salvar3(Object usuario) {
+	public Usuario salvar2(Object usuario) {
 		List<Usuario> usuario2 = new ArrayList(((LinkedHashMap<String, String>)usuario).keySet());
 		return usuarioNegocio.salvar(usuario2.get(0));
 	}
