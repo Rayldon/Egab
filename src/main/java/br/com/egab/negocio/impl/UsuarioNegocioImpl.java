aa3da54130c7bd91bf3a6f33ae3476ae2a76dfe6
Rayldon <r4yldon@gmail.com>
2018-11-04 16:42:47 -0200

    teste2

--- src/main/java/br/com/egab/negocio/impl/UsuarioNegocioImpl.java
+++ src/main/java/br/com/egab/negocio/impl/UsuarioNegocioImpl.java
@@ -36,7 +36,7 @@ public class UsuarioNegocioImpl implements UsuarioNegocio{
 		return usuarioPersistencia.getEntityManager().merge(usuario);
 	}
 	
-	public Usuario salvar3(Usuario usuario){		
+	public Usuario salvar2(Usuario usuario){		
 		return usuarioPersistencia.getEntityManager().merge(usuario);
 	}
 }
